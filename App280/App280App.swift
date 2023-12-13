497ef7483af86b94cd6a9bdd4b87399b566410a7
spustoff <vyacheslavpustov05@gmail.com>
2023-12-13 15:33:26 +0300

    Initial Commit

---
+++ App280/App280App.swift
@@ -0,0 +1,17 @@
+//
+//  App280App.swift
+//  App280
+//
+//  Created by Вячеслав on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct App280App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
